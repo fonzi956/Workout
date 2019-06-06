760ccd96b116dc73c151b6c2a08b80dcc386d106
fonzi956 <alfonsolozoya@ymail.com>
2019-06-06 14:24:44 -0500

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var sec = 420;
+setTimeout(countDown,1000);
+console.log(sec);
+function countDown(){
+   sec--;
+   if(sec > 0){
+      setTimeout(countDown,1000);
+   }
+
+   console.log(n);
+}
+
+/*
+30 and 10 seconds notications then 5 seconds countdonw to the next workout
+
+Seated Ab Circles Left x 60 seconds
+Seated Ab Circles Right x 60 seconds
+Drunken Mountain Climbers x 60 seconds
+Rest 30 seconds
+Marching Planks x 60 seconds
+Scissors x 60 seconds
+Starfish Crunch x 30 seconds
+Rest 30 seconds
+Russian V Tuck Twists x 30 seconds
+
+1) 5x20-25 reps push ups 
+2) 4x25 reps air squats 
+
+3) 5x30sec holds 
+4) 4x8/12 leg raises
+*/
