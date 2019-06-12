884163e7eb9e93b1c6c748552441608a397591f4
Alfonso Lozoya <alfonsolozoya@ymail.com>
2019-06-12 09:25:02 -0500

    change resting array index to 0 in sec > 0 and added 31 meaning starting at 30 second for 1 min workout

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function typeWorkOut(min, sec){
    if(min == 1) return ["Seated Ab Circles Right x 60 seconds", "Drunken Mountain Climbers x 60 seconds", "1"];
    if(min == 2) return ["Drunken Mountain Climbers x 60 seconds", "REST", "1"];
    if((min == 3 && sec <= 30)) return ["REST", "Marching Planks x 60 seconds", "30"];
-   if((min == 3 && sec >= 30) || (min == 4 && sec <= 30)) return ["Marching Planks x 60 seconds", "Scissors x 60 seconds", "1"];
-   if((min == 4 && sec >= 30 ) || (min == 5 && sec <= 30)) return ["Scissors x 60 seconds", "Starfish Crunch x 30 seconds", "1"];
+   if((min == 3 && sec >= 30) || (min == 4 && sec <= 30)) return ["Marching Planks x 60 seconds", "Scissors x 60 seconds", "31"];
+   if((min == 4 && sec >= 30 ) || (min == 5 && sec <= 30)) return ["Scissors x 60 seconds", "Starfish Crunch x 30 seconds", "31"];
    if(min == 5 && sec >= 30)  return ["Starfish Crunch x 30 seconds", "REST", "30"];
    if( min == 6) return ["REST", "Russian V Tuck Twists x 30 seconds", "30"];
    if(min == 6 && sec >= 30) return ["Russian V Tuck Twists x 30 seconds", "30"];
@@ -30,6 +30,16 @@ function countUp(){
       console.log("10");
       play("ten");
    }
+   // starting at the 30 seconds for 1 min workout
+   if(resting[2] == "31"&& sec == 19){
+      console.log("10");
+      play("ten");
+
+   }
+   if(resting[2] == "31"&& sec == 59){
+      console.log("30");
+      play("thirty");
+   }
    if(resting[2] == "30" && sec == 15){
       console.log("fithteen");
       play("fithteen");
@@ -39,7 +49,8 @@ function countUp(){
    document.getElementById("h2").innerHTML = resting[0] + " <br>next is "+ resting[1];
    document.getElementById("p1").innerHTML = min+" : "+sec;
    if(sec > 0){
-      if((min > 0 && (sec >= 0 && sec <= 1)) && resting[1] != "REST"){
+      //console.log(resting[0] == "REST");
+      if((min > 0 && (sec >= 0 && sec <= 1)) && resting[0] != "REST"){
          document.getElementById("h2").innerHTML = resting[0] + " <br>next is "+ resting[1];
          setTimeout(countUp,5000);
          //go
@@ -52,7 +63,6 @@ function countUp(){
 
 function play(mus) {
    var song = "WOR/"+mus+".mp3";
-   console.log(song);
    var audio = new Audio(song);
    audio.play();
 }
@@ -65,7 +75,7 @@ http://127.0.0.1:5500/index.html
 Seated Ab Circles Left x 60 seconds
 Seated Ab Circles Right x 60 seconds
 Drunken Mountain Climbers x 60 seconds
-Rest 30 seconds
+Rest 30 seconds 3:30
 Marching Planks x 60 seconds
 Scissors x 60 seconds
 Starfish Crunch x 30 seconds
